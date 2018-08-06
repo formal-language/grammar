18f9c36d47d70ac51f97d1bdf819005d23955dcc
Aurélien Ooms <aurelien.ooms@gmail.com>
2018-08-06 09:56:06 +0200

    Remove recursion in ast.flatten: fix #4

--- src/ast/flatten.js
+++ src/ast/flatten.js
@@ -1,4 +1,46 @@
-export default function* flatten ( tree ) {
-	if ( tree.type === 'leaf' ) yield tree ;
-	else for ( const child of tree.children ) yield* flatten( child ) ;
+import { iter } from '@aureooms/js-itertools' ;
+
+export default function* flatten ( root ) {
+
+	// assert root.type === 'node'
+
+	const stack = [
+		{
+			type : 'node' ,
+			nonterminal : root.nonterminal ,
+			production : root.production ,
+			children : iter( root.children ) ,
+		}
+	] ;
+
+	while ( stack.length !== 0 ) {
+
+		const tree = stack.pop();
+
+		const { done , value } = tree.children.next();
+
+		if ( !done ) {
+
+			stack.push(tree);
+
+			const child = value ;
+
+			if ( child.type === 'leaf' ) yield child ;
+
+			else {
+
+				const newchild = {
+					type : 'node' ,
+					nonterminal : child.nonterminal ,
+					production : child.production ,
+					children : iter(child.children) ,
+				} ;
+
+				stack.push(newchild);
+
+			}
+
+		}
+
+	}
 }
