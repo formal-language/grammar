850fb387aeabd123a125f3678b09fc188247f5e5
Aurélien Ooms <aurelien.ooms@gmail.com>
2018-08-08 18:45:21 +0200

    explicit await in _parse_lazy:shallow_materialize

--- src/ll1/_parse_lazy.js
+++ src/ll1/_parse_lazy.js
@@ -12,7 +12,7 @@ import _children_next_lazy from './_children_next_lazy' ;
  */
 export default function _parse_lazy ( eof , grammar , table , rule , tape , nonterminal , productionid ) {
 
-	const shallow_materialize = async x => _children_next_lazy(eof, grammar, table, tape, x) ;
+	const shallow_materialize = async x => await _children_next_lazy(eof, grammar, table, tape, x) ;
 
 	const children = rmap( shallow_materialize , rule )[Symbol.asyncIterator]() ;
 
