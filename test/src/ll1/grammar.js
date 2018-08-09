8e8f45564fca6debb3ea4c99e12d8c25f5dcbae7
Aurélien Ooms <aurelien.ooms@gmail.com>
2018-08-09 14:13:31 +0200

    move ll1.is(G) === false tests in their own file

--- test/src/ll1/grammar.js
+++ test/src/ll1/grammar.js
@@ -1,12 +1,10 @@
 import test from 'ava' ;
 
-import tape from '@aureooms/js-tape' ;
-
-import { sorted , list , map , enumerate } from '@aureooms/js-itertools' ;
+import { sorted , list , map } from '@aureooms/js-itertools' ;
 
 import { increasing } from '@aureooms/js-compare' ;
 
-import { grammar , ast , ll1 } from '../../../src' ;
+import { grammar , ll1 } from '../../../src' ;
 
 const {
 	_first ,
@@ -143,52 +141,6 @@ test( 'Dragon Book (2006) page 71' , t => {
 	t.true(ll1.is(G));
 
 });
-
-test( 'Dragon Book (2006) page 49' , t => {
-
-	const start = '0';
-	const eof = '$';
-
-	const productions = [
-		[ // right : 0 (start)
-			[ 1 , '==' , 0 ] ,
-			[ 1 ] ,
-		] ,
-		list( map( l => [ `=${l}` ] , 'abcdefghijklmnopqrstuvwxyz' ) ) , // letter : 1
-	] ;
-
-	const G = grammar.from( { start , eof , productions } ) ;
-
-	t.false(ll1.is(G));
-
-	//const table = compile(start, eof, G);
-
-	//const tokens = tape.fromString('a=b=c');
-
-	//const tree = parse(start, eof, G, table, tokens);
-
-}) ;
-
-test( 'Dragon Book (2006) page 258 exercise 4.6.6' , t => {
-
-	const start = 'S';
-	const eof = '$';
-
-	const productions = {
-		"S" : [
-			[ "&S" , "&A" ] ,
-			[ "&A" ]
-		] ,
-		"A" : [
-			[ "=a" ]
-		]
-	} ;
-
-	const G = grammar.from( { start , eof , productions } ) ;
-
-	t.false(ll1.is(G));
-
-}) ;
 
 test( 'chain' , t => {
 
---
+++ test/src/ll1/not-ll1.js
@@ -0,0 +1,51 @@
+import test from 'ava' ;
+
+import { list , map } from '@aureooms/js-itertools' ;
+
+import { grammar , ll1 } from '../../../src' ;
+
+test( 'Dragon Book (2006) page 49' , t => {
+
+	const start = '0';
+	const eof = '$';
+
+	const productions = [
+		[ // right : 0 (start)
+			[ 1 , '==' , 0 ] ,
+			[ 1 ] ,
+		] ,
+		list( map( l => [ `=${l}` ] , 'abcdefghijklmnopqrstuvwxyz' ) ) , // letter : 1
+	] ;
+
+	const G = grammar.from( { start , eof , productions } ) ;
+
+	t.false(ll1.is(G));
+
+	//const table = compile(start, eof, G);
+
+	//const tokens = tape.fromString('a=b=c');
+
+	//const tree = parse(start, eof, G, table, tokens);
+
+}) ;
+
+test( 'Dragon Book (2006) page 258 exercise 4.6.6' , t => {
+
+	const start = 'S';
+	const eof = '$';
+
+	const productions = {
+		"S" : [
+			[ "&S" , "&A" ] ,
+			[ "&A" ]
+		] ,
+		"A" : [
+			[ "=a" ]
+		]
+	} ;
+
+	const G = grammar.from( { start , eof , productions } ) ;
+
+	t.false(ll1.is(G));
+
+}) ;
