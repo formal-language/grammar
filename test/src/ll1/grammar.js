82d32f8c98c7da9536ee9f0769257d5ec9d052a6
Aurélien Ooms <aurelien.ooms@gmail.com>
2018-08-09 14:08:29 +0200

    add test for ll1.is

--- test/src/ll1/grammar.js
+++ test/src/ll1/grammar.js
@@ -169,6 +169,27 @@ test( 'Dragon Book (2006) page 49' , t => {
 
 }) ;
 
+test( 'Dragon Book (2006) page 258 exercise 4.6.6' , t => {
+
+	const start = 'S';
+	const eof = '$';
+
+	const productions = {
+		"S" : [
+			[ "&S" , "&A" ] ,
+			[ "&A" ]
+		] ,
+		"A" : [
+			[ "=a" ]
+		]
+	} ;
+
+	const G = grammar.from( { start , eof , productions } ) ;
+
+	t.false(ll1.is(G));
+
+}) ;
+
 test( 'chain' , t => {
 
 	const start = '0';
