formal-language/grammar

commit b41970567a89bc7cbea097b16224352c072dea2c

:shirt: refactor: Initial lint.

src/ll1/_first.js
@@ -2,9 +2,10 @@ import {map} from '@iterable-iterator/map';
 import {filter} from '@iterable-iterator/filter';
 import {any} from '@iterable-iterator/reduce';
 
-import { setadd , setaddall } from '../util/index.js' ;
+import setadd from '../util/setadd.js';
+import setaddall from '../util/setaddall.js';
 
-import { EW } from '../grammar/index.js';
+import EW from '../grammar/EW.js';
 
 /**
  * Computes FIRST table for grammar nonterminals.
@@ -12,41 +13,36 @@ import { EW } from '../grammar/index.js';
  * @param productions
  * @returns {Map}
  */
-export default function _first ( productions ) {
-
-	const FIRST = new Map( map( key => [ key , new Set() ] , productions.keys() ) ) ;
+export default function _first(productions) {
+	const FIRST = new Map(map((key) => [key, new Set()], productions.keys()));
 
 	let updated = true;
 
 	while (updated) {
-
 		updated = false;
 
 		for (const [i, rules] of productions) {
-
 			for (const rule of rules.values()) {
-
 				let read = true;
 
-				for ( const x of rule ) {
+				for (const x of rule) {
+					if (!read) break;
+					read = false;
 
-					if (!read) break; read = false;
-
-					if ( x.type === 'leaf' ) updated |= setadd( FIRST.get(i), x.terminal);
+					if (x.type === 'leaf') updated ||= setadd(FIRST.get(i), x.terminal);
 					else {
-						updated |= setaddall(FIRST.get(i), filter(y => y !== EW, FIRST.get(x.nonterminal)));
-						read |= any(map(y => y === EW, FIRST.get(x.nonterminal)));
+						updated ||= setaddall(
+							FIRST.get(i),
+							filter((y) => y !== EW, FIRST.get(x.nonterminal)),
+						);
+						read ||= any(map((y) => y === EW, FIRST.get(x.nonterminal)));
 					}
-
 				}
 
-				if (read) updated |= setadd(FIRST.get(i), EW);
+				if (read) updated ||= setadd(FIRST.get(i), EW);
 			}
-
 		}
-
 	}
 
 	return FIRST;
-
 }

src/ll1/_follow.js
@@ -2,11 +2,10 @@ import {map} from '@iterable-iterator/map';
 import {filter} from '@iterable-iterator/filter';
 import {enumerate} from '@iterable-iterator/zip';
 
-import { setaddall } from '../util/index.js' ;
+import setaddall from '../util/setaddall.js';
 
-import first from './first.js' ;
-
-import { EW } from '../grammar/index.js';
+import EW from '../grammar/EW.js';
+import first from './first.js';
 
 /**
  * Computes the FOLLOW table for all nonterminals.
@@ -15,19 +14,21 @@ import { EW } from '../grammar/index.js';
  * @param {Map} productions - The productions map.
  * @returns {Map} The FOLLOW table.
  */
-export default function _follow ( FIRST , productions ) {
+export default function _follow(FIRST, productions) {
+	const FOLLOW = new Map(map((i) => [i, new Set()], productions.keys()));
 
-	const FOLLOW = new Map( map( i => [ i , new Set() ] , productions.keys() ) ) ;
+	const couldbelast = new Map(map((i) => [i, new Set()], productions.keys()));
 
-	const couldbelast = new Map( map( i => [ i , new Set() ] , productions.keys() )) ;
-
-	for (const [ A , rules ] of productions) {
+	for (const [A, rules] of productions) {
 		for (const rule of rules.values()) {
-			for (const [ pos , B ] of enumerate(rule)) {
-				if (B.type === 'leaf') continue ;
-				const fi = first(FIRST, rule.slice(pos+1));
+			for (const [pos, B] of enumerate(rule)) {
+				if (B.type === 'leaf') continue;
+				const fi = first(FIRST, rule.slice(pos + 1));
 				if (fi.has(EW)) couldbelast.get(A).add(B.nonterminal);
-				setaddall(FOLLOW.get(B.nonterminal), filter( x => x !== EW , fi));
+				setaddall(
+					FOLLOW.get(B.nonterminal),
+					filter((x) => x !== EW, fi),
+				);
 			}
 		}
 	}
@@ -37,11 +38,10 @@ export default function _follow ( FIRST , productions ) {
 		updated = false;
 		for (const A of productions.keys()) {
 			for (const B of couldbelast.get(A)) {
-				updated |= setaddall(FOLLOW.get(B), FOLLOW.get(A));
+				updated ||= setaddall(FOLLOW.get(B), FOLLOW.get(A));
 			}
 		}
 	}
 
 	return FOLLOW;
-
 }

src/util/setaddall.js
@@ -1,4 +1,4 @@
-import setadd from './setadd.js' ;
+import setadd from './setadd.js';
 
 /**
  * Adds all elements of an iterable to a set and returns true if the set has
@@ -8,8 +8,8 @@ import setadd from './setadd.js' ;
  * @param {Iterable} iterable - The iterable of elements to add to the set.
  * @returns {Boolean} Whether <code>set</code> has changed.
  */
-export default function setaddall ( set , iterable ) {
+export default function setaddall(set, iterable) {
 	let changed = false;
-	for (const element of iterable) changed |= setadd(set, element);
+	for (const element of iterable) changed ||= setadd(set, element);
 	return changed;
 }

doc/scripts/header.js
@@ -1,34 +1,30 @@
-var domReady = function(callback) {
-	var state = document.readyState ;
-    if ( state === 'interactive' || state === 'complete' ) {
-		callback() ;
-	}
-	else {
+const domReady = function (callback) {
+	const state = document.readyState;
+	if (state === 'interactive' || state === 'complete') {
+		callback();
+	} else {
 		document.addEventListener('DOMContentLoaded', callback);
 	}
-} ;
+};
 
-
-domReady(function(){
-
-	var projectname = document.createElement('a');
+domReady(function () {
+	const projectname = document.createElement('a');
 	projectname.classList.add('project-name');
 	projectname.text = 'formal-language/grammar';
-	projectname.href = './index.html' ;
+	projectname.href = './index.html';
 
-	var header = document.getElementsByTagName('header')[0] ;
-	header.insertBefore(projectname,header.firstChild);
+	const header = document.querySelectorAll('header')[0];
+	header.insertBefore(projectname, header.firstChild);
 
-	var testlink = document.querySelector('header > a[data-ice="testLink"]') ;
-	testlink.href = 'https://coveralls.io/github/formal-language/grammar' ;
-	testlink.target = '_BLANK' ;
+	const testlink = document.querySelector('header > a[data-ice="testLink"]');
+	testlink.href = 'https://coveralls.io/github/formal-language/grammar';
+	testlink.target = '_BLANK';
 
-	var searchBox = document.querySelector('.search-box');
-	var input = document.querySelector('.search-input');
+	const searchBox = document.querySelector('.search-box');
+	const input = document.querySelector('.search-input');
 
-	// active search box when focus on searchBox.
-	input.addEventListener('focus', function(){
+	// Active search box when focus on searchBox.
+	input.addEventListener('focus', function () {
 		searchBox.classList.add('active');
 	});
-
 });

src/ast/Children.js
@@ -1,18 +1,15 @@
-import rmap from './rmap.js' ;
-import _children_exhaust from './_children_exhaust.js' ;
+import _children_exhaust from './_children_exhaust.js';
 
 export default class Children {
-
-	constructor ( iterator, length ) {
-		this.iterator = iterator ;
-		this.length = length ;
+	constructor(iterator, length) {
+		this.iterator = iterator;
+		this.length = length;
 	}
 
-	async* [Symbol.asyncIterator]() {
-		for await ( const node of this.iterator ) {
-			yield node ;
-			if ( node.type === 'node' ) await _children_exhaust( node.children ) ;
+	async *[Symbol.asyncIterator]() {
+		for await (const node of this.iterator) {
+			yield node;
+			if (node.type === 'node') await _children_exhaust(node.children);
 		}
 	}
-
 }

src/ast/_children_exhaust.js
@@ -1,4 +1,4 @@
-import { anyIterator } from '../util/index.js' ;
+import anyIterator from '../util/anyIterator.js';
 
 /**
  * Exhausts the input children iterator.
@@ -6,33 +6,28 @@ import { anyIterator } from '../util/index.js' ;
  * @param {Iterator} children - The input children iterator.
  *
  */
-export default async function _children_exhaust ( children ) {
+export default async function _children_exhaust(children) {
+	// For await ( const child of children ) {
+	// if ( child.type === 'node' ) await _children_exhaust( child.children ) ;
+	// }
 
-	//for await ( const child of children ) {
-		//if ( child.type === 'node' ) await _children_exhaust( child.children ) ;
-	//}
+	const stack = [anyIterator(children)];
 
-	const stack = [ anyIterator(children) ] ;
-
-	while ( stack.length !== 0 ) {
-
+	while (stack.length > 0) {
 		const it = stack.pop();
 
-		const { done , value } = await it.next();
+		// eslint-disable-next-line no-await-in-loop
+		const {done, value} = await it.next();
 
-		if ( !done ) {
-
+		if (!done) {
 			stack.push(it);
 
-			const child = value ;
+			const child = value;
 
-			if ( child.type === 'node' ) {
-				const grandchildren = anyIterator(child.children) ;
+			if (child.type === 'node') {
+				const grandchildren = anyIterator(child.children);
 				stack.push(grandchildren);
 			}
-
 		}
-
 	}
-
 }

src/ast/cmap.js
@@ -1,4 +1,4 @@
-//import _children_exhaust from './_children_exhaust' ;
+// Import _children_exhaust from './_children_exhaust' ;
 
 /**
  * Applies a given callable to each of the child of a given children async iterable.
@@ -7,11 +7,9 @@
  * @param {AsyncIterable} children - The input children.
  * @returns {AsyncIterator}
  */
-export default async function* cmap ( callable , children ) {
-
-	for await ( const child of children ) {
-		const node = await callable( child ) ;
-		yield node ;
+export default async function* cmap(callable, children) {
+	for await (const child of children) {
+		const node = await callable(child);
+		yield node;
 	}
-
 }

src/ast/flatten.js
@@ -1,49 +1,44 @@
-import { anyIterator } from '../util/index.js' ;
+import anyIterator from '../util/anyIterator.js';
 
-function getCount ( object ) {
-	if ( object instanceof Function || object.length === undefined ) return Infinity ;
-	else return object.length ;
+function getCount(object) {
+	if (object instanceof Function || object.length === undefined)
+		return Number.POSITIVE_INFINITY;
+	return object.length;
 }
 
-export default async function* flatten ( root ) {
-
-	// assert root.type === 'node'
+export default async function* flatten(root) {
+	// Assert root.type === 'node'
 
 	const stack = [
-		{ // no need to use the exhaustive iterator since flatten exhausts the whole subtree
-			children : anyIterator(root.children) ,
-			count : getCount(root.children) ,
-		}
-	] ;
+		{
+			// No need to use the exhaustive iterator since flatten exhausts the whole subtree
+			children: anyIterator(root.children),
+			count: getCount(root.children),
+		},
+	];
 
-	while ( stack.length !== 0 ) {
+	while (stack.length > 0) {
+		const tree = stack.pop();
 
-		const tree = stack.pop() ;
+		// eslint-disable-next-line no-await-in-loop
+		const {done, value} = await tree.children.next();
 
-		const { done , value } = await tree.children.next() ;
+		if (!done) {
+			if (--tree.count > 0)
+				// Prunes left side of the tree
+				stack.push(tree);
 
-		if ( !done ) {
+			const child = value;
 
-			if ( --tree.count > 0 ) // prunes left side of the tree
-			stack.push(tree);
-
-			const child = value ;
-
-			if ( child.type === 'leaf' ) yield child ;
-
+			if (child.type === 'leaf') yield child;
 			else {
-
 				const it = {
-					children : anyIterator(child.children) ,
-					count : getCount(child.children) ,
-				} ;
+					children: anyIterator(child.children),
+					count: getCount(child.children),
+				};
 
 				stack.push(it);
-
 			}
-
 		}
-
 	}
-
 }

commit 74133352933a59e10d53c975a29eb13d0ba590a5

:adhesive_bandage: fix: Do not shortcut setadd/setaddall side effects.
This bug was introduced in b419705. We used |= before because we tried
to be clever. Then, to satisfy the newly introduced linter, we tried to
be clever again by thinking replacing |= by ||= would not cause any
problems.

src/ll1/_first.js
@@ -29,17 +29,19 @@ export default function _first(productions) {
 					if (!read) break;
 					read = false;
 
-					if (x.type === 'leaf') updated ||= setadd(FIRST.get(i), x.terminal);
+					if (x.type === 'leaf')
+						updated = setadd(FIRST.get(i), x.terminal) || updated;
 					else {
-						updated ||= setaddall(
-							FIRST.get(i),
-							filter((y) => y !== EW, FIRST.get(x.nonterminal)),
-						);
+						updated =
+							setaddall(
+								FIRST.get(i),
+								filter((y) => y !== EW, FIRST.get(x.nonterminal)),
+							) || updated;
 						read ||= any(map((y) => y === EW, FIRST.get(x.nonterminal)));
 					}
 				}
 
-				if (read) updated ||= setadd(FIRST.get(i), EW);
+				if (read) updated = setadd(FIRST.get(i), EW) || updated;
 			}
 		}
 	}

src/ll1/_follow.js
@@ -38,7 +38,7 @@ export default function _follow(FIRST, productions) {
 		updated = false;
 		for (const A of productions.keys()) {
 			for (const B of couldbelast.get(A)) {
-				updated ||= setaddall(FOLLOW.get(B), FOLLOW.get(A));
+				updated = setaddall(FOLLOW.get(B), FOLLOW.get(A)) || updated;
 			}
 		}
 	}

src/util/setaddall.js
@@ -10,6 +10,9 @@ import setadd from './setadd.js';
  */
 export default function setaddall(set, iterable) {
 	let changed = false;
-	for (const element of iterable) changed ||= setadd(set, element);
+	for (const element of iterable) {
+		changed = setadd(set, element) || changed;
+	}
+
 	return changed;
 }