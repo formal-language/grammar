c18d619585f9d0532e48a6682aa4b927c62f2f2f
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-11-23 16:52:25 +0100

    :books: docs(Children): Document constructor parameters.

--- src/ast/Children.js
+++ src/ast/Children.js
@@ -1,6 +1,10 @@
 import _children_exhaust from './_children_exhaust.js';
 
 export default class Children {
+	/**
+	 * @param {AsyncIterableIterator} iterator
+	 * @param {number} length
+	 */
 	constructor(iterator, length) {
 		this.iterator = iterator;
 		this.length = length;
